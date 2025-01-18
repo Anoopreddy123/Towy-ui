0394f8fe89aa7e8ef60cfbd92a37dd3a18e4e2bb
anoopreddykunta <anoopreddy240@gmail.com>
2025-01-18 15:39:18 -0800

    Changing the API end point

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api'
+    baseURL: 'https://towy-backend-a1nr3ts21-anoops-projects-e49f75e4.vercel.app'
 });
 
 // Add token to requests
